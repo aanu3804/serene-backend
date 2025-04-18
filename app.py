bc6fc3cfe7e08eae0fd71d9e66edfdeb8a175a7e
Anand sai Lattala <anandsai2004@gmail.com>
2025-04-18 17:20:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, origins=["https://serene-pied.vercel.app"], supports_credentials=True)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 UPLOAD_FOLDER = "uploads"
 AUDIO_FOLDER = "static/audio"
